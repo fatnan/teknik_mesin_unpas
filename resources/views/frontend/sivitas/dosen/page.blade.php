b1edb99c5cd632cfa4ee32c49b972e9624406a92
pulangpergih <pulangpergih@gmail.com>
2021-05-26 23:09:23 +0700

    add align right and card detail dosen

--- resources/views/frontend/sivitas/dosen/page.blade.php
+++ resources/views/frontend/sivitas/dosen/page.blade.php
@@ -36,7 +36,7 @@
                                 Sivitas
                             </li>
                             <li class="breadcrumb-item">
-                                Dosen
+                               <a href="{{ url('/sivitas/dosen/index')}}" style="color:#333">Dosen</a>
                             </li>
                             <li class="breadcrumb-item active">
                                 Ir. Toto Supriyono, MT.
@@ -45,33 +45,59 @@
                     </nav>
                 </div>
             </div>
-          </div>
-         {{--  card  --}}
-          <div class="row justify-content-center">
-            <div class="col-sm-8 pl-lg-3">
-              <div class="section section-details-01">
-                <div class="card-wrapper">
-                  <div class="row justify-content-center">
-                    <div class="card col-xs-6 col-md-4">
-                      <img class="card-img-top p-2" src="/images/toto-supriyono.jpg">
-                      <div class="list">
-                        <ul style="list-style-type:none;">
-                          <li>Nama :</li>
-                          <li>Jabtan :</li>
-                          <li>Email :</li>
-                          <li>Bidang Keahlian :</li>
-                          <p> Pendidikan </p>
-                          <li>S1</li>
-                          <li>S2 :</li>
-                          <li>S3 :</li>
-                          </ul>
-                        </div>
+            {{--  card  --}}
+            <div class="row mt-5">
+              <div class="col-sm pl-lg-3 mt-5">
+                  <div class="section section-details-01">
+                      <div class="card-deck-wrapper">
+                          <div class="card-deck justify-content-center">
+                              <div class="card card-inverse-01 col-xs-6 col-sm-4 ">
+                                  <div class="card-block">
+                                      <img class="card-img-top p-2" src="/images/toto-supriyono.jpg" alt="Card image cap">
+                                      <div class="card-body">
+                                        <ul class="list-unstyled" style="margin-left: -10px">
+                                          <li>Nama :</li>
+                                          <li>Jabtan :</li>
+                                          <li>Email :</li>
+                                          <li>Bidang Keahlian :</li>
+                                          <p> Pendidikan </p>
+                                          <li>S1 :</li>
+                                          <li>S2 :</li>
+                                          <li>S3 :</li>
+                                          </ul>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
                       </div>
-                    </div>
                   </div>
-                </div>
-              </div>  
+              </div>
+              <div class="col-lg-2">
+                  <div class="section section-right-01" style="margin-top: 50px">
+                      <ul class="list-unstyled">
+                          <li><a href="">Mahasiswa</a>
+                          </li>
+                          <hr>
+                          <li><a href="">Alumni</a>
+                          </li>
+                          <hr>
+                          <li><a href="">Dosen</a>
+                          </li>
+                          <hr>
+                          <li><a href="">Staff</a>
+                          </li>
+                          <hr>
+                          <li><a href="">Mitra</a>
+                          </li>
+                          <hr>
+                          <li><a href="">Himpunan Mahasiswa</a>
+                          </li>
+                          <hr>
+                      </ul>
+                  </div>
+              </div>
           </div>
+        </div>
       </section>
     </main>
 @endsection
