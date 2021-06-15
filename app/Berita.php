3f259ac8d90ab2462b4dbc016a4b77d8da2039b3
Fatnan Ahmad Thawsan <fatnan.a@smt-id.com>
2021-06-15 13:56:03 +0700

    fix berita crud

--- app/Berita.php
+++ app/Berita.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Berita extends Model
 {
-    protected $fillable = ['judul','content','updated_at','created_at'];
+    protected $fillable = ['judul','isi','updated_at','created_at'];
     protected $table = 'berita';
 }
